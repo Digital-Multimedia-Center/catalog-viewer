ca4b6272b0bf43ef28b38b212195c2f7b867d2cf
Swefton <amrit.madhav.srivastava@gmail.com>
2026-02-20 14:37:49 -0500

    skeleton for a filtering system

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,18 @@
 import { getEnrichedGames } from "@/lib/mongodb";
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+
 import GameCard from "./components/GameCard/GameCard.js";
 
 export default async function Home() {
@@ -7,6 +21,28 @@ export default async function Home() {
   return (
   <main style={{ padding: "40px", maxWidth: "900px", margin: "0 auto"}}>
       <h1 style={{ marginBottom: "30px" }}>Enriched Collection</h1>
+      <Input />
+
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline">Platform</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuGroup>
+            <DropdownMenuLabel>Modern</DropdownMenuLabel>
+            <DropdownMenuItem>PlayStation 5</DropdownMenuItem>
+            <DropdownMenuItem>PlayStation 4</DropdownMenuItem>
+            <DropdownMenuItem>Xbox One Series X</DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuLabel>Retro</DropdownMenuLabel>
+            <DropdownMenuItem>Nintendo DS</DropdownMenuItem>
+            <DropdownMenuItem>PlayStation Portable</DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
       {games.map((game) => (
         <GameCard key={game._id} game={game} />
       ))}
